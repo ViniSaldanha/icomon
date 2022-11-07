f6d8976d91908b2b8ec027c208c991839e07fbbf
Vinicius Saldanha <viniosald@gmail.com>
2022-11-07 11:23:20 -0300

    limpeza de codigo

--- resources/views/cadastros/ba/carrega-csv.js
+++ resources/views/cadastros/ba/carrega-csv.js
@@ -1,38 +1,10 @@
-
-(function(){
-    const fileInput = document.querySelector('#arquivo-csv');
-    fileInput.addEventListener('change', event => {
-        const files = event.target.files;
-        uploadFileNew(files[0]);
+async function renderTableFromFile(){
+    let formData = new FormData();
+    formData.append('arquivo-csv', fileupload.files[0]); // AQUI ACESSA DIRETAMENTE O ID DO ELEMENTO
+    const response = await fetch('/cadastros/ba/renderTableFromFile', {
+        method: 'POST',
+        body: formData
     });
 
-    const uploadFile = file => {
-        const URL = '/cadastros/ba/loadFile';
-        const request = new XMLHttpRequest();
-        const formData = new FormData();
-        formData.append('arquivo-csv', file);
-
-        request.open('POST', URL, true);
-        request.onload = () => {
-            if( request.status === 200){
-                document.getElementById('dados-csv').innerHTML = request.responseText;
-            }
-        };
-        
-        request.send(formData);
-    }
-
-    
-    // EXEMPLO COM ES6
-    async function uploadFileNew(file){
-        let formData = new FormData();
-        formData.append('arquivo-csv', file);
-        let response = await fetch('/cadastros/ba/loadFile', {
-            method: 'POST',
-            body: formData
-        });
-
-        document.getElementById('dados-csv').innerHTML = await response.text();
-    }
-
-})();
+    document.getElementById('dados-csv').innerHTML = await response.text();
+}
