2ef97bc1bbc80ff46996ec308d5ce610d899e559
Fabiano <f4314n0@gmail.com>
2022-11-07 09:45:53 -0300

    limpeza de codigo

--- resources/views/cadastros/ba/carrega-csv.js
+++ resources/views/cadastros/ba/carrega-csv.js
@@ -1,4 +1,3 @@
-// EXEMPLO COM ES6
 async function renderTableFromFile(){
     let formData = new FormData();
     formData.append('arquivo-csv', fileupload.files[0]); // AQUI ACESSA DIRETAMENTE O ID DO ELEMENTO
@@ -9,30 +8,3 @@ async function renderTableFromFile(){
 
     document.getElementById('dados-csv').innerHTML = await response.text();
 }
-
-
-(function(){
-
-    const uploadFileNormal = file => {
-        const URL = '/cadastros/ba/renderTableFromFile';
-        const request = new XMLHttpRequest();
-        const formData = new FormData();
-        formData.append('arquivo-csv', file);
-
-        request.open('POST', URL, true);
-        request.onload = () => {
-            if( request.status === 200){
-                document.getElementById('dados-csv').innerHTML = request.responseText;
-            }
-        };
-        
-        request.send(formData);
-    }
-
-    /* const fileInput = document.querySelector('#arquivo-csv');
-    fileInput.addEventListener('change', event => {
-        const files = event.target.files;
-        uploadFileNew(files[0]);
-    }); */
-
-})();
