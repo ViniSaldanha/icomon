1b907f456da81b41a87ead99d95bbd12a16e098d
Fabiano <f4314n0@gmail.com>
2023-01-07 12:51:55 -0300

    melhoria para edição de registros, selecionar item gravado na base

--- App/Controller/Components/Options.php
+++ App/Controller/Components/Options.php
@@ -5,18 +5,28 @@ use \App\Utils\View;
 use \App\Model\Entity\Options as EntityOptions;
 
 class Options{
-    public static function loadOptions($tag){
+    public static function loadOptions($tag, $selected = null){
         $itens = '';
         $where = 'tag = "'.$tag.'"';
         $results = EntityOptions::getOptions($where);
 
         while($opt = $results->fetchObject(EntityOptions::class)){
             $itens .= View::render('components/options/option', [
+                'selected' => self::handleSelectedItem($opt->value, $selected),
                 'value' => $opt->value,
                 'text'  => $opt->text
             ]);
         }
 
         return $itens;
+    }
+
+    private static function handleSelectedItem($option, $item){
+        $result = '';
+        if($option == $item){
+            $result = 'selected';
+        }
+
+        return $result;
     }
 }
